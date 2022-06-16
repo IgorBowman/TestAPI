a6decb757231deb8b3d20215ba34333a9b5b49c8
IgorBowman <xxx@gmaol.com>
2022-06-16 18:51:52 +0500

    [upd] register custom user model

--- core/api/admin.py
+++ core/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from api.models import MyUser
 
-admin.site.register(MyUser)
+admin.site.register(MyUser, UserAdmin)
--- core/core/settings.py
+++ core/core/settings.py
@@ -103,3 +103,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ]
 }
+
+
+AUTH_USER_MODEL = 'api.MyUser'
